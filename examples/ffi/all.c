3c77bb7bcf5be180c0d93e123f6f4ba34aaa8607
lung <62560250+if-not-nil@users.noreply.github.com>
2026-05-10 15:36:33 +0300

    c example cleaned up a bit

--- examples/ffi/all.c
+++ examples/ffi/all.c
@@ -1,19 +1,19 @@
 #include "../../revo.h"
-#include <string.h>
-#include <stdlib.h>
 #include <regex.h>
+#include <stdlib.h>
+#include <string.h>
 
 /// > echo(s) -> string
-R_SIG(echo) {
+void echo(void *vm, size_t argc, RevoData *argv, RevoData *out_result) {
   if (argc < 1 || argv[0].tag != revo_string) {
-    *out_result = R_NIL();
+    *out_result = (RevoData){.tag = revo_atom, .value = ra_ok};
     return;
   }
   *out_result = argv[0];
 }
 
 /// > strlen(s) -> number
-R_SIG(strlen_fn) {
+void strlen_fn(void *vm, size_t argc, RevoData *argv, RevoData *out_result) {
   if (argc < 1 || argv[0].tag != revo_string) {
     *out_result = (RevoData){.tag = revo_number, .value = 0};
     return;
@@ -24,7 +24,7 @@ R_SIG(strlen_fn) {
 }
 
 /// > add(a, b) -> number
-R_SIG(add) {
+void add(void *vm, size_t argc, RevoData *argv, RevoData *out_result) {
   if (argc < 2) {
     *out_result = (RevoData){.tag = revo_number, .value = 0};
     return;
@@ -37,7 +37,7 @@ R_SIG(add) {
 
 /// > type(x) -> number
 /// returns type tag: 0=number, 1=string, 2=atom, etc
-R_SIG(type) {
+void type(void *vm, size_t argc, RevoData *argv, RevoData *out_result) {
   if (argc < 1) {
     *out_result = R_NIL();
     return;
@@ -48,26 +48,28 @@ R_SIG(type) {
 
 /// > regex(pattern, text) -> number
 /// returns 1 if pattern matches, 0 if not
-R_SIG(regex) {
+void regex(void *vm, size_t argc, RevoData *argv, RevoData *out_result) {
   if (argc < 2 || argv[0].tag != revo_string || argv[1].tag != revo_string) {
     *out_result = (RevoData){.tag = revo_number, .value = 0};
     return;
   }
   const char *pattern = (const char *)(uintptr_t)argv[0].value;
   const char *text = (const char *)(uintptr_t)argv[1].value;
-  
+
   regex_t regex;
   if (regcomp(&regex, pattern, REG_EXTENDED | REG_NOSUB) != 0) {
     regfree(&regex);
     *out_result = (RevoData){.tag = revo_number, .value = 0};
     return;
   }
-  
+
   int match = regexec(&regex, text, 0, NULL, 0);
   regfree(&regex);
-  
+
   double result = (match == 0) ? 1.0 : 0.0;
   *out_result = (RevoData){.tag = revo_number, .value = *(uint64_t *)&result};
 }
 
-R_EXPORT({"echo", echo}, {"strlen_fn", strlen_fn}, {"add", add}, {"type", type}, {"regex", regex})
+__attribute__((visibility("default"))) const RevoBinding revo_bindings[] = {
+    {"echo", echo}, {"strlen_fn", strlen_fn}, {"add", add},
+    {"type", type}, {"regex", regex},         {((void *)0), ((void *)0)}};
